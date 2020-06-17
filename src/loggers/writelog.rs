8d197114ca52422edd62c0adbb67ceb9fde4ef6b
Thomas Forgione <thomas@forgione.fr>
2020-06-17 16:48:01 +0200

    Adds module feature

--- src/loggers/writelog.rs
+++ src/loggers/writelog.rs
@@ -18,6 +18,7 @@ pub struct WriteLogger<W: Write + Send + 'static> {
     level: LevelFilter,
     config: Config,
     writable: Mutex<W>,
+    modules: Vec<String>,
 }
 
 impl<W: Write + Send + 'static> WriteLogger<W> {
@@ -35,9 +36,14 @@ impl<W: Write + Send + 'static> WriteLogger<W> {
     /// let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create("my_rust_bin.log").unwrap());
     /// # }
     /// ```
-    pub fn init(log_level: LevelFilter, config: Config, writable: W) -> Result<(), SetLoggerError> {
+    pub fn init(
+        log_level: LevelFilter,
+        config: Config,
+        writable: W,
+        modules: Vec<String>,
+    ) -> Result<(), SetLoggerError> {
         set_max_level(log_level.clone());
-        set_boxed_logger(WriteLogger::new(log_level, config, writable))
+        set_boxed_logger(WriteLogger::new(log_level, config, writable, modules))
     }
 
     /// allows to create a new logger, that can be independently used, no matter what is globally set.
@@ -56,18 +62,48 @@ impl<W: Write + Send + 'static> WriteLogger<W> {
     /// let file_logger = WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_bin.log").unwrap());
     /// # }
     /// ```
-    pub fn new(log_level: LevelFilter, config: Config, writable: W) -> Box<WriteLogger<W>> {
+    pub fn new(
+        log_level: LevelFilter,
+        config: Config,
+        writable: W,
+        modules: Vec<String>,
+    ) -> Box<WriteLogger<W>> {
         Box::new(WriteLogger {
             level: log_level,
             config: config,
             writable: Mutex::new(writable),
+            modules,
         })
+    }
+
+    fn includes_module(&self, module_path: &str) -> bool {
+        // If modules is empty, include all module paths
+        if self.modules.is_empty() {
+            return true;
+        }
+        // if a prefix of module_path is in `self.modules`, it must
+        // be located at the first location before
+        // where module_path would be.
+        match self
+            .modules
+            .binary_search_by(|module| module.as_str().cmp(&module_path))
+        {
+            Ok(_) => {
+                // Found exact module: return true
+                true
+            }
+            Err(0) => {
+                // if there's no item which would be located before module_path, no prefix is there
+                false
+            }
+            Err(i) => is_submodule(&self.modules[i - 1], module_path),
+        }
     }
 }
 
 impl<W: Write + Send + 'static> Log for WriteLogger<W> {
     fn enabled(&self, metadata: &Metadata<'_>) -> bool {
-        metadata.level() <= self.level
+        metadata.level() <= self.level && self.includes_module(metadata.target())
     }
 
     fn log(&self, record: &Record<'_>) {
@@ -94,4 +130,26 @@ impl<W: Write + Send + 'static> SharedLogger for WriteLogger<W> {
     fn as_log(self: Box<Self>) -> Box<dyn Log> {
         Box::new(*self)
     }
+}
+
+fn is_submodule(parent: &str, possible_child: &str) -> bool {
+    // Treat as bytes, because we'll be doing slicing, and we only care about ':' chars
+    let parent = parent.as_bytes();
+    let possible_child = possible_child.as_bytes();
+
+    // a longer module path cannot be a parent of a shorter module path
+    if parent.len() > possible_child.len() {
+        return false;
+    }
+
+    // If the path up to the parent isn't the same as the child,
+    if parent != &possible_child[..parent.len()] {
+        return false;
+    }
+
+    // Either the path is exactly the same, or the sub module should have a "::" after
+    // the length of the parent path. This prevents things like 'a::bad' being considered
+    // a submodule of 'a::b'
+    parent.len() == possible_child.len()
+        || possible_child.get(parent.len()..parent.len() + 2) == Some(b"::")
 }
